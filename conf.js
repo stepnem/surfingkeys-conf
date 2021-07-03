3910bee8efcc78ae7e403862cba7670e3f407b20
Štěpán Němec <stepnem@gmail.com>
2021-07-03 22:02:49 +0200

    WIP reddit keys

--- conf.js
+++ conf.js
@@ -47,4 +47,30 @@ const searchleader = "a"
 util.processMaps(keys.maps, keys.aliases, siteleader)
 util.processCompletions(completions, searchleader)
 
+if (typeof KeyboardUtils !== "undefined") {
+  if (_isDomainApplicable(/reddit\.com/)) {
+    [".", "f", "h", "j", "k", "K", "J", "<Ctrl-z>", "<Shift-Enter>", "<Enter>",
+      "x", "=", "-", "+", "_", "[", "]", "c", "C", "l", "L", "a", "z", "r", "u",
+      "<Alt-/>", "gi", "gm", "gu", "gf", "gF", "gn", "gp"].forEach((key) => {
+      const enk = KeyboardUtils.encodeKeystroke(key)
+      if (Normal.mappings.find(enk)) Normal.mappings.remove(enk)
+      else {
+        // eslint-disable-next-line guard-for-in
+        for (const k in Mode.specialKeys) {
+          const idx = Mode.specialKeys[k].indexOf(key)
+          if (idx !== -1) {
+            Mode.specialKeys[k].splice(idx, 1)
+          }
+        }
+      }
+    })
+    mapkey("b", "#1Open a link in current tab", () => {
+      Hints.create("", Hints.dispatchMouseClick, { tabbed: false })
+    })
+    mapkey("w", "#1Open a link in active new tab", () => {
+      Hints.create("", Hints.dispatchMouseClick, { tabbed: true, active: true })
+    })
+  }
+}
+
 module.exports = { siteleader, searchleader }
